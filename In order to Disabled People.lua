0b3e44ce585ed94d2ac2a1d2f6dcc3c55a816493
Konkee <113286276+Konkee007@users.noreply.github.com>
2026-01-11 21:11:10 +0700

    Update In order to Disabled People.lua

--- In order to Disabled People.lua
+++ In order to Disabled People.lua
@@ -21,6 +21,9 @@ LocalPlayer:WaitForChild("PlayerGui")
 SendNotification("Maitem Service", "PlayerGui Loaded", 3)
 
 --- Settings ---
+sendwebhook = getgenv().Config.SendWebhook
+auto_relog = getgenv().Config.AutoRelog
+auto_check_dead = getgenv().Config.AutoCheckDead
 auto_redeem_all_code = getgenv().Config.AutoRedeemAllCodes
 sound_muted = getgenv().Config.SoundMuted
 auto_join_vip_server = getgenv().Config.AutoJoinVipServer
@@ -30,6 +33,7 @@ black_screen = getgenv().Config.BlackScreen
 load_script = getgenv().Config.LoadScript
 kaitun = getgenv().Config.Kaitun
 ---------------
+
 
 SendNotification("Maitem Service", "Config Loaded", 3)
 
@@ -106,6 +110,129 @@ if game.PlaceId == 3978370137 then
             end
 
             SendNotification("Maitem Service", "All Codes Redeemed", 3)
+        end
+    end)
+
+    task.spawn(function()
+        if auto_check_dead then
+            SendNotification("Maitem Service","AutoCheckDead has Enabled", 3)
+            while task.wait(1) do 
+                 if LocalPlayer.Character.Humanoid.Health <= 1 then
+                    if auto_relog == false then 
+                       SendNotification("Maitem Service",LocalPlayer.Name.." has dead. Rejoining ", 1) 
+                    end
+                    task.wait(2)
+                    SendNotification("Maitem Service","Rejoined", 1) 
+                    game:GetService("TeleportService"):Teleport(1730877806,LocalPlayer)
+                 end
+            end
+        end
+    end)
+
+    task.spawn(function()
+        if type(auto_relog) == "number" then 
+            SendNotification("Maitem Service","AutoRelog has Enabled", 10)
+            SendNotification("Maitem Service","AutoRelog will rejoin you once "..auto_relog.." minutes have passed.", 10)
+            local totalTime = auto_relog * 60
+            local startTime = os.clock()
+            local relogging = false
+            while task.wait(1) do 
+                 if os.clock() - startTime >= totalTime then
+                    if relogging == false then 
+                        relogging = true
+                        SendNotification("Maitem Service",auto_relog.." minutes elapsed. Relogging...", 3)
+                        task.wait(1)
+                        if load_script then 
+                            LocalPlayer:Kick("[Kicked by Maitem Service] In order to Relogging. (Working with Feral Auto Rejoin)")
+                        else
+                            game:GetService("ReplicatedStorage").Events.KnockedOut:FireServer("self")
+                        end
+                    end
+                end
+            end
+        end
+    end)
+
+    task.spawn(function()
+        if type(sendwebhook) == "string" then 
+            SendNotification("Maitem Service","SendWebhook has Enabled", 20)
+            local Time_Minute = 10
+            
+            while task.wait(Time_Minute * 60) do 
+                if not kaitun then 
+                    task.spawn(function()
+                        local webhookUrl = sendwebhook
+                        local PlayerLevel = game:GetService("ReplicatedStorage"):WaitForChild("Stats"..game:GetService("Players").LocalPlayer.Name).Stats.Level.Value
+                        local PlayerMoney = game:GetService("ReplicatedStorage"):WaitForChild("Stats"..game:GetService("Players").LocalPlayer.Name).Stats.Peli.Value
+                        Legbait_CleanValue = 0
+                        local success,errmsg = pcall(function()
+                            if game:GetService("Players").LocalPlayer.PlayerGui.FishingBaitGui then
+                                if game:GetService("Players").LocalPlayer.PlayerGui.FishingBaitGui.Main.List["Legendary Fish Bait"] then
+                                    local rawValue = game:GetService("Players").LocalPlayer.PlayerGui.FishingBaitGui.Main.List["Legendary Fish Bait"].ItemCount.Text
+                                    local cleanValue = string.sub(rawValue,2)
+                                    Legbait_CleanValue = cleanValue
+                                end
+                            end
+                        end)
+                    
+                        if errmsg then
+                            local tool = game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Merchants Banana Rod") or game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Fishing Rod")
+                            if tool then
+                                game:GetService("Players").LocalPlayer.Character.Humanoid:EquipTool(tool)
+                                if game:GetService("Players").LocalPlayer.PlayerGui.FishingBaitGui.Main.List["Legendary Fish Bait"] then
+                                    local rawValue = game:GetService("Players").LocalPlayer.PlayerGui.FishingBaitGui.Main.List["Legendary Fish Bait"].ItemCount.Text
+                                    local cleanValue = string.sub(rawValue,2)
+                                    Legbait_CleanValue = cleanValue
+                                else
+                                    Legbait_CleanValue = "Cannot be specified"
+                                end
+                            else
+                                Legbait_CleanValue = "Cannot be specified"
+                            end
+                        end
+                    
+                    
+                        local data = {
+                            username = game:GetService("Players").LocalPlayer.Name,
+                            content = nil,
+                            embeds = {
+                                {
+                                description = "⬆️ **Level :** ```"..PlayerLevel.."```\n💰 **Peli : **```"..PlayerMoney.."```\n🐟 **Legendary Fish Bait :** ```"..Legbait_CleanValue.."```",
+                                color = 14370059,
+                                timestamp = DateTime.now():ToIsoDate(),
+                                }
+                            },
+                        }
+                        local newData = game:GetService("HttpService"):JSONEncode(data)
+                        local headers = {["content-type"] = "application/json"}
+                        request = http_request or request or HttpPost
+                        local sendWebhook = {Url = webhookUrl, Body = newData, Method = "POST", Headers = headers}
+                        request(sendWebhook)
+                    end)
+                else
+                    task.spawn(function()
+                        local webhookUrl = sendwebhook
+                        local PlayerLevel = game:GetService("ReplicatedStorage"):WaitForChild("Stats"..game:GetService("Players").LocalPlayer.Name).Stats.Level.Value
+                        local PlayerMoney = game:GetService("ReplicatedStorage"):WaitForChild("Stats"..game:GetService("Players").LocalPlayer.Name).Stats.Peli.Value
+                        local data = {
+                            username = game:GetService("Players").LocalPlayer.Name.." (Kaitun)",
+                            content = nil,
+                            embeds = {
+                                {
+                                description = "⬆️ **Level :** ```"..PlayerLevel.."```\n💰 **Peli : **```"..PlayerMoney.."```",
+                                color = 14370059,
+                                timestamp = DateTime.now():ToIsoDate(),
+                                }
+                            },
+                        }
+                        local newData = game:GetService("HttpService"):JSONEncode(data)
+                        local headers = {["content-type"] = "application/json"}
+                        request = http_request or request or HttpPost
+                        local sendWebhook = {Url = webhookUrl, Body = newData, Method = "POST", Headers = headers}
+                        request(sendWebhook)
+                    end)
+                end
+            end
         end
     end)
 
@@ -300,4 +427,3 @@ if game.PlaceId == 3978370137 then
         end
     end
 end
-
